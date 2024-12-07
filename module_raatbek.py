f3213a186599d941af1939ae21a9f037ebdf6288
Az1mov02 <raatbek.azimov1712@gmail.com>
2024-12-07 19:17:26 +0600

    Raatbek: Добавил файл module.py

---
+++ module_raatbek.py
@@ -0,0 +1,17 @@
+STUDENTS_LIST = [
+    "Esen M.",
+    "Altynai",
+    "Azamat T.",
+    "Kylych S.",
+    "Arsen K.",
+]
+
+MAX_CORE = 30
+
+STUDENTS_GRADES = (
+    (15, MAX_CORE),
+    (12, MAX_CORE),
+    (30, MAX_CORE),
+    (1, MAX_CORE),
+    (99, MAX_CORE),
+)
